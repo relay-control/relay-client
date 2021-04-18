f1c72f0333e3ac1fe0208c1cb4c58d9596f3c63b
Lombra <Lombra@users.noreply.github.com>
2021-04-18 22:12:27 +0200

    Put style definitions where they belong.

--- src/web/scripts/styles.js
+++ src/web/scripts/styles.js
@@ -263,3 +263,139 @@ class ControlStyle extends Style {
 		// this.control = control.control
 	}
 }
+
+class SubStyle extends Style {
+	constructor(selector, parent) {
+		super(parent.selector + selector)
+		this.parent = parent
+	}
+}
+
+class LabelStyle extends SubStyle {
+	constructor(selector, parent) {
+		super(' .label' + selector, parent)
+	}
+	
+	get styleProperties() {
+		return super.styleProperties.concat([
+			'color',
+			'font',
+			'textShadow',
+		])
+	}
+	
+	get containerStyle() {
+		return this.getStyleRule(`${this.selector}`)
+	}
+	
+	get controlStyle() {
+		return this.getStyleRule(`${this.selector} span`)
+	}
+	
+	set anchor(anchor) {
+		super.anchor = anchor
+		this.setAreaStyle('--parent', anchor.parent)
+	}
+	
+	set color(color) {
+		this.setControlStyle('color', color)
+	}
+	
+	set font(font) {
+		// this.setControlStyle('color', parseColor(font.color, font.alpha))
+		if (font.family) this.setControlStyle('font-family', font.family)
+		this.setControlStyle('font-size', parseLength(font.size))
+	}
+	
+	set textShadow(shadows) {
+		let textShadows = []
+		for (let [shadow] of shadows) {
+			let textShadow = []
+			textShadow.push(parseLength(shadow.offsetX || 0), parseLength(shadow.offsetY || 0))
+			if ('blurRadius' in shadow) textShadow.push(parseLength(shadow.blurRadius))
+			if (shadow.color) textShadow.push(parseColor(shadow.color, shadow.alpha))
+			textShadows.push(textShadow.join(' '))
+		}
+		this.setControlStyle('text-shadow', textShadows.join(', '))
+	}
+}
+
+class TextLabelStyle extends LabelStyle {
+	constructor(parent) {
+		super('.text', parent)
+	}
+}
+
+class IconLabelStyle extends LabelStyle {
+	constructor(parent) {
+		super('.icon', parent)
+	}
+}
+
+class ValueLabelStyle extends LabelStyle {
+	constructor(parent) {
+		super('.value', parent)
+	}
+}
+
+class ActiveLabelStyle extends SubStyle {
+	constructor(parent) {
+		super('.active .label', parent)
+		// this.containerSelector = `${this.selector}`
+		this.controlSelector = `${this.selector}`
+	}
+	
+	get styleProperties() {
+		return super.styleProperties.concat([
+			'font',
+		])
+	}
+	
+	set font(font) {
+		this.setControlStyle('color', parseColor(font.color, font.alpha))
+		if (font.family) this.setControlStyle('font-family', font.family)
+		this.setControlStyle('font-size', parseLength(font.size))
+	}
+}
+
+class ActiveStyle extends SubStyle {
+	constructor(parent) {
+		super('.active', parent)
+	}
+}
+
+class SliderThumbStyle extends SubStyle {
+	constructor(parent) {
+		super('::-webkit-slider-thumb', parent)
+	}
+	
+	get containerStyle() {
+		return this.parent.getStyleRule(`${this.selector}`)
+	}
+	
+	get controlStyle() {
+		return this.parent.getStyleRule(`${this.selector}`)
+	}
+	
+	set height(height) {
+		this.parent.setControlStyle('--thumb-height', parseLength(height))
+	}
+}
+
+class SliderTrackStyle extends SubStyle {
+	constructor(parent) {
+		super('::-webkit-slider-runnable-track', parent)
+	}
+	
+	get containerStyle() {
+		return this.parent.getStyleRule(`${this.selector}`)
+	}
+	
+	get controlStyle() {
+		return this.parent.getStyleRule(`${this.selector}`)
+	}
+	
+	set height(height) {
+		this.parent.setControlStyle('--track-height', parseLength(height))
+	}
+}
