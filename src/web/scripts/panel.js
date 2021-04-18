e891c3bb5b6dbe1a0a07f7fb784780c9eb7649d7
Lombra <Lombra@users.noreply.github.com>
2021-04-18 22:56:09 +0200

    Refactor panel building.

--- src/web/scripts/panel.js
+++ src/web/scripts/panel.js
@@ -17,6 +17,94 @@ class Panel {
 	constructor() {
 		this.element = document.getElementById('panel')
 	}
+
+	build(panelData) {
+		// create a separate stylesheet for dynamic style rules
+		Stylesheet.create()
+		
+		this.element.style = null
+		this.element.style.display = 'none'
+		if (panel.background)
+			this.background = panel.background
+		this.rows = panel.grid.rows
+		this.columns = panel.grid.columns
+		
+		/* validate grid size and control placement */
+		
+		if (panel.assets) {
+			for (let [asset, type] of panel.assets) {
+				let file = recon.getAssetPath(asset.file)
+				switch (type) {
+					case 'Image':
+						this.loadImage(file)
+						break
+					case 'Font':
+						this.loadFont(asset.family, file)
+						break
+					case 'Script':
+						this.loadScript(file)
+						break
+				}
+			}
+		}
+		
+		if (panel.templates) {
+			for (let [template, tag] of panel.templates) {
+				// map each template to a CSS class
+				let selector = template.name
+				if (tag !== 'Control') selector += '.' + tag.toLowerCase()
+				let style = new TemplateStyle(selector)
+				style.apply('.' + template)
+				if (template.active) {
+					style.applyActive(template.active)
+				}
+			}
+		}
+		
+		let usedVJoyDevices = []
+		let usedVJoyDeviceButtons = {}
+		let usedVJoyDeviceAxes = {}
+		
+		for (let [view] of panel.views) {
+			let v = this.createView(view)
+			v.build()
+		}
+		
+		// request devices
+		recon.connect(usedVJoyDevices)
+		 .then(devices => {
+			this.setView(1)
+			
+			this.show()
+			
+			let warnings = []
+			for (let device of devices) {
+				if (!device.acquired) {
+					warnings.push(`Unable to acquire device ${device.id}`)
+				} else {
+					if (usedVJoyDeviceButtons[device.id] > device.numButtons) {
+						warnings.push(`Device ${device.id} has ${device.numButtons} buttons but this panel uses ${usedVJoyDeviceButtons[device.id]}`)
+					}
+					if (usedVJoyDeviceAxes[device.id]) {
+						for (let axis of usedVJoyDeviceAxes[device.id]) {
+							if (!device.axes[axis]) {
+								warnings.push(`Requested axis ${axis} not enabled on device ${device.id}`)
+							}
+						}
+					}
+				}
+			}
+			if (warnings.length > 0) {
+				menuViewModel.deviceInfoDialog.data(warnings)
+				menuViewModel.deviceInfoDialog.show()
+			}
+		 })
+		 .catch(err => {
+			console.error(err)
+			menuViewModel.modalDialog.show(`Connection refused`)
+			menuViewModel.currentPanel(null)
+		 })
+	}
 	
 	show() {
 		Promise.allSettled(this.assets)
@@ -107,95 +195,6 @@ class Stylesheet {
 	}
 }
 
-function buildPanel(panel) {
-	// create a separate stylesheet for dynamic style rules
-	Stylesheet.create()
-	
-	let p = new Panel()
-	p.element.style = null
-	p.element.style.display = 'none'
-	if (panel.background)
-		p.background = panel.background
-	p.rows = panel.grid.rows
-	p.columns = panel.grid.columns
-	
-	/* validate grid size and control placement */
-	
-	if (panel.assets) {
-		for (let [asset, type] of panel.assets) {
-			let file = recon.getAssetPath(asset.file)
-			switch (type) {
-				case 'Image':
-					p.loadImage(file)
-					break
-				case 'Font':
-					p.loadFont(asset.family, file)
-					break
-				case 'Script':
-					p.loadScript(file)
-					break
-			}
-		}
-	}
-	
-	if (panel.templates) {
-		for (let [template, tag] of panel.templates) {
-			// map each template to a CSS class
-			let selector = template.name
-			if (tag !== 'Control') selector += '.' + tag.toLowerCase()
-			let style = new TemplateStyle(selector)
-			style.apply('.' + template)
-			if (template.active) {
-				style.applyActive(template.active)
-			}
-		}
-	}
-	
-	let usedVJoyDevices = []
-	let usedVJoyDeviceButtons = {}
-	let usedVJoyDeviceAxes = {}
-	
-	for (let [view] of panel.views) {
-		let v = p.createView(view)
-		v.build()
-	}
-	
-	// request devices
-	recon.connect(usedVJoyDevices)
-	 .then(devices => {
-		p.setView(1)
-		
-		p.show()
-		
-		let warnings = []
-		for (let device of devices) {
-			if (!device.acquired) {
-				warnings.push(`Unable to acquire device ${device.id}`)
-			} else {
-				if (usedVJoyDeviceButtons[device.id] > device.numButtons) {
-					warnings.push(`Device ${device.id} has ${device.numButtons} buttons but this panel uses ${usedVJoyDeviceButtons[device.id]}`)
-				}
-				if (usedVJoyDeviceAxes[device.id]) {
-					for (let axis of usedVJoyDeviceAxes[device.id]) {
-						if (!device.axes[axis]) {
-							warnings.push(`Requested axis ${axis} not enabled on device ${device.id}`)
-						}
-					}
-				}
-			}
-		}
-		if (warnings.length > 0) {
-			menuViewModel.deviceInfoDialog.data(warnings)
-			menuViewModel.deviceInfoDialog.show()
-		}
-	 })
-	 .catch(err => {
-		console.error(err)
-		menuViewModel.modalDialog.show(`Connection refused`)
-		menuViewModel.currentPanel(null)
-	 })
-}
-
 function loadPanel(panelName) {
 	// if (panelName !== currentPanel) {
 		let panel = document.getElementById('panel')
@@ -212,9 +211,10 @@ function loadPanel(panelName) {
 	saveSetting('lastPanel', panelName)
 	
 	recon.getPanel(panelName)
-	 .then(panel => {
-		// console.log(panel)
-		buildPanel(panel)
+	 .then(panelData => {
+		// console.log(panelData)
+		let panel = new Panel()
+		panel.build(panelData)
 	 })
 	 .catch(err => {
 		if (err instanceof FileNotFoundError) {
